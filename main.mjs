ecf7f308ba8828e3cfeb8f0702cb19e2c6820026
upkanet <jeandamien.louise@gmail.com>
2022-02-16 15:44:19 +0100

    refactor:rid of node and env

--- main.mjs
+++
@@ -1,9 +0,0 @@
-import ffi from 'ffi-napi';
-// import ref from 'ref-napi';
-
-
-const libpath = "dll/x64/alp42.dll";
-
-const DMD = ffi.Library(libpath,{
-    'AlpDevAlloc': ['int',['long','int','pointer']]
-});
